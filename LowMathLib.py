e7b90fc5576c0a4feb29e2e9115bec1303fdfba5
Draxuxe <louis.filloux53@gmail.com>
2024-11-08 14:52:07 +0100

    Ajout du logreg et add fonction dans LowMathLib

--- LowMathLib.py
+++ LowMathLib.py
@@ -19,13 +19,13 @@ class LowMathLib:
 
     @staticmethod
     def ft_mean(values):
-            return sum(values) / len(values) if values else 0
+        return sum(values) / len(values) if values else 0
 
     @staticmethod
     def ft_std_dev(values):
-            mean_val = LowMathLib.ft_mean(values)
-            variance = sum((x - mean_val) ** 2 for x in values) / (len(values) - 1) if len(values) > 1 else 0
-            return variance ** 0.5
+        mean_val = LowMathLib.ft_mean(values)
+        variance = sum((x - mean_val) ** 2 for x in values) / (len(values) - 1) if len(values) > 1 else 0
+        return variance ** 0.5
     
     @staticmethod
     def ft_median(values):
@@ -71,15 +71,13 @@ class LowMathLib:
             return None
 
         return numerator / (denominator_x * denominator_y)
-    
+
     @staticmethod
-    def min_max(values):
-        min_ = min(values)
-        max_ = max(values)
-        for index in range(len(values)):
-            if (max_ - min_) != 0:
-                values[index] = ((values[index] - min_) / (max_ - min_))
+    def cost(X, y, theta):
+        m = len(y)
+        h = sigmoid(X.dot(theta))
+        return (-1 / m) * (y.T.dot(np.log(h)) + (1 - y).T.dot(np.log(1 - h)))
 
     @staticmethod
     def sigmoid(z):
-        return (1 / (1 + np.exp(-z)))
+        return 1 / (1 + np.exp(-z))
--- logreg_predict.py
+++ logreg_predict.py
@@ -6,24 +6,23 @@ import os.path
 import LowMathLib
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
-class Train:
+class Predict:
     def __init__(self):
         self.data = {}
-        self.weights = {}
         self.argv = sys.argv
+        self.predictions = []
         self.lib = LowMathLib.LowMathLib()
         self.classes = ["Arithmancy", "Astronomy", "Herbology", "Defense Against the Dark Arts", "Divination", "Muggle Studies", "Ancient Runes", "History of Magic", "Transfiguration", "Potions", "Care of Magical Creatures", "Charms", "Flying"]
 
     def parser(self):
-        if len(self.argv) < 2 or len(self.argv) > 2:
-            print("[Command] ./python3 logreg_predict.py [ARG REQUIRED] [PATH TO]/dataset_train.csv")
+        if len(self.argv) < 2 or len(self.argv) > 3:
+            print("[Command] ./python3 logreg_predict.py [ARG REQUIRED] [PATH TO]/dataset_test.csv [ARG REQUIRED] weighs file")
             sys.exit()
-        elif len(self.argv) == 2:
-            if os.path.basename(self.argv[1]) == "dataset_train.csv":
-                if os.path.exists(self.argv[1]):
-                    if os.access(self.argv[1], os.R_OK):
+        elif len(self.argv) == 3:
+            if os.path.basename(self.argv[1]) == "dataset_test.csv":
+                if os.path.exists(self.argv[1]) or os.path.exists(self.argv[2]):
+                    if os.access(self.argv[1], os.R_OK) and os.access(self.argv[2], os.R_OK):
                         self.data = pd.read_csv(self.argv[1])
                         for classe in self.classes:
                             parsed_data = []
@@ -40,9 +39,18 @@ class Train:
                     print("[Error] Le fichier est introuvable. Etes-vous sûrs du chemin d'accès?")
                     sys.exit()
             else:
-                print("[Error] Le nom du fichier doit être: 'dataset_train.csv'")
+                print("[Error] Le nom du fichier doit être: 'dataset_test.csv'")
                 sys.exit()
 
+    def predict(self):
+        X = self.data[self.classes].to_numpy()
+        X = np.hstack((np.ones((X.shape[0], 1)), X))
+        lib = LowMathLib.LowMathLib()
+        with open(self.argv[2], "r") as f:
+            weights = json.load(f)
+        for i in range(X.shape[0]):
+            probs = {cls: lib.sigmoid(np.dot(X[i], np.array(weights))) for cls, weights in weights.items()}
+            self.predictions.append(max(probs, key=probs.get))
 
     def normalization(self):
         lib = LowMathLib.LowMathLib()
@@ -50,34 +58,17 @@ class Train:
             min_val = self.lib.ft_min(self.data[classe])
             max_val = self.lib.ft_max(self.data[classe])
             self.data[classe] = (self.data[classe] - min_val) / (max_val - min_val)
-
-    def train(self):
-        lib = LowMathLib.LowMathLib()
-        X = self.data[self.classes].to_numpy()
-        X = np.hstack((np.ones((X.shape[0], 1)), X))
-        houses = self.data['Hogwarts House'].unique()
-        for house in houses:
-            y = (self.data['Hogwarts House'] == house).astype(int).values
-            weight = np.zeros(X.shape[1])
-            weight = gradient_descent(X, y, weight, 0.025, 1500)
-            self.weights[house] = weight.tolist()
     
     def output(self):
-        with open("weights.json", "w") as f:
-            json.dump(self.weights, f)
-
-def gradient_descent(X, y, weight, alpha, iterations):
-    lib = LowMathLib.LowMathLib()
-    m = len(y)
-    for i in range(iterations):
-        h = lib.sigmoid(X.dot(weight))
-        gradient = (1 / m) * X.T.dot(h - y)
-        weight -= alpha * gradient
-    return weight
+        with open('houses.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(['Index', 'Hogwarts House'])
+            for i in range(len(self.predictions)):
+                writer.writerow([str(i), self.predictions[i]])
 
 if __name__ == "__main__":
-    TRAIN = Train()
-    TRAIN.parser()
-    TRAIN.normalization()
-    TRAIN.train()
-    TRAIN.output()
+    PREDICT = Predict()
+    PREDICT.parser()
+    PREDICT.normalization()
+    PREDICT.predict()
+    PREDICT.output()
